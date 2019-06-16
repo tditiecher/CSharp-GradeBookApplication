bbaa632a0174cf3a6dcc5ae247f8ac8159e8a0e7
Taco Ditiecher <taco@tdev.org>
2019-06-16 21:41:46 +0200

    2. Creating New GradeBookTypes

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,20 @@
+﻿using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook : BaseGradeBook
+    {
+        public StandardGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Standard;
+        }
+    }
+
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Ranked;
+        }
+    }
+}
